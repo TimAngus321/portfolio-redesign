97194955ea1464539ae09be76386367e7d7ae0b6
Timothy Angus <timangus321@gmail.com>
2020-09-11 16:52:33 +0200

    contact form working, sensitive data hidden, style and responsiveness needs work

--- src/screens/App/screens/Contact/components/Contact.js
+++ src/screens/App/screens/Contact/components/Contact.js
@@ -1,141 +1,34 @@
 import React from 'react';
-import { init, emailjs } from 'emailjs-com';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { SMTPClient } from 'emailjs';
+import emailjs from 'emailjs-com';
 
+export default function ContactUs() {
 
-export default class extends React.Component {
-    constructor(props) {
-        super(props);
+  const user_id = process.env.REACT_APP_EMAILJS_USER_ID;
+  const template_id = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const service_id = process.env.REACT_APP_EMAILJS_SERVICE_ID;
 
-        this.state = {
-            name: "",
-            email: "",
-            subject: "",
-            feedback: "",
-        }
-    }
+  function sendEmail(e) {
+    e.preventDefault();
 
-    handleInputChange(event) {
-        event.preventDefault();
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
-
-    this.setState({ [name]: value })
-    }
-
-    sendMessage(event) {
-      event.preventDefault();
-
-      const client = new SMTPClient({
-        user: 'user',
-        password: 'password',
-        host: 'smtp.your-email.com',
-        ssl: true,
-    });
-
-  const templateParams = {
-        from_name: this.state.name + " (" + this.state.email + ")",
-        to_name: "Timothy Angus",
-        feedback: this.state.feedback
-      };
-  emailjs
-        .send("gmail", "portfoliositecontact", 'service_cqvp8hl', 'template_8ahjk2c', templateParams)
-        .then(
-          function(response) {
-            toast.success("Your message has successfully sent!", {
-              position: toast.POSITION.BOTTOM_CENTER
-            });
-            console.log("SUCCESS!", response.status, response.text);
-          },
-          function(err) {
-            toast.error("Your message was not able to be sent");
-          }
-        );
-  this.setState({
-        name: "",
-        email: "",
-        message: ""
+    emailjs.sendForm(service_id, template_id, e.target, user_id)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
       });
-    }
+  }
 
-  
-    
-    render() {
-        return(
-            <div className="contact-container">
-            <h1>Contact Me</h1>
-            <form 
-                className="contact-form"
-                id={this.props.id}
-                name={this.props.name}
-                method={this.props.method}
-                action={this.props.action}
-                onSubmit={handleSubmit}
-            >
-            <textarea 
-            id="name"
-            name="name"
-            onChange={this.handleInputChange.bind(this)}
-            placeholder="Your name"
-            required
-            value={this.state.name}
-            style={{ width: "100%" }}
-            rows={1}
-            >
-            </textarea>
-
-            <textarea 
-            id="email"
-            name="email"
-            onChange={this.handleInputChange.bind(this)}
-            placeholder="Your email address"
-            required
-            value={this.state.email}
-            style={{ width: "100%" }}
-            rows={1}
-            >
-            </textarea>
-
-            <textarea 
-            id="subject"
-            name="subject"
-            onChange={this.handleInputChange.bind(this)}
-            placeholder="Subject"
-            required
-            value={this.state.subject}
-            style={{ width: "100%" }}
-            rows={1}
-            >
-            </textarea>
-            
-            <textarea 
-            id="feedback"
-            name="feedback"
-            onChange={this.handleInputChange.bind(this)}
-            placeholder="Your message"
-            required
-            value={this.state.feedback}
-            style={{ width: "100%" }}
-            rows={1}
-            >
-            </textarea>
-            <input
-            type="button"
-            value="Send"
-            className="contact-form button"
-            style={{
-              fontFamily: "Amatic SC",
-              fontSize: "20px",
-              color: "orange"
-            }}
-            onClick={this.sendMessage.bind(this)}
-          />
-            </form>
-            </div>
-        )
-    }
+  return (
+    <div className="contact-container">
+    <h1 className="contact-title">Contact Me</h1>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number"/>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="email" name="email" placeholder="Email"/>
+        <input type="subject" name="subject" placeholder="Subject"/>
+        <textarea name="message" placeholder="Message"/>
+        <input className="contact-btn" type="submit" value="Send" />
+      </form>
+    </div>
+  );
 }
-

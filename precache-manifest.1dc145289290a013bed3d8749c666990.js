7bef96b2c3b2f2a8bb409189cf18c2b696ae9953
Timothy Angus <timangus321@gmail.com>
2020-09-22 16:52:56 +0200

    Updates

--- precache-manifest.1dc145289290a013bed3d8749c666990.js
+++ precache-manifest.1dc145289290a013bed3d8749c666990.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2d4a68f0ba37f04f71c7e7bec84754ea",
+    "revision": "ae6f6b541790973c39bed5d9cdbaaea7",
     "url": "/portfolio-redesign/index.html"
   },
   {
-    "revision": "22de2723016b66e84e96",
-    "url": "/portfolio-redesign/static/css/main.fd16ed66.chunk.css"
+    "revision": "e290dbc14d4f68ed92b4",
+    "url": "/portfolio-redesign/static/css/main.61ea96d2.chunk.css"
   },
   {
     "revision": "582fba0d33b31a29aafd",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-redesign/static/js/2.8cc6c4b7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "22de2723016b66e84e96",
-    "url": "/portfolio-redesign/static/js/main.bdf10464.chunk.js"
+    "revision": "e290dbc14d4f68ed92b4",
+    "url": "/portfolio-redesign/static/js/main.b226e630.chunk.js"
   },
   {
     "revision": "3992262ecde2be220795",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-redesign/precache-manifest.98319cafe244373dc5822a2e8afcbfae.js"
+  "/portfolio-redesign/precache-manifest.1dc145289290a013bed3d8749c666990.js"
 );
 
 self.addEventListener('message', (event) => {

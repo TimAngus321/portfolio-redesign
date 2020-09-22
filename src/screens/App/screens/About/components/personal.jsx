d9d989992220e75d0d41d7e7f78e597b70e7ea51
Timothy Angus <timangus321@gmail.com>
2020-09-22 16:49:13 +0200

    progress on about page

--- src/screens/App/screens/About/components/personal.jsx
+++ src/screens/App/screens/About/components/personal.jsx
@@ -11,7 +11,16 @@ class Personal extends Component {
 
   render() {
     return <div className="about-me-info">
-      I love travelling, music, rock climbing and coding.
+      I love travelling and I've visited many coutries around Europe, Asia and and states in the U.S.A
+      <br/><br/>
+      I'm a big fan of music and love live music and creating music. 
+      <br/><br/>
+      I always love trying new sports and recently I have fallen back in love with:
+      Rock climbing, running and biking. 
+      <br/><br/>
+      To keep my mind sharp I enjoy reading news and dev articles, novels and watching documenteries (not just coding tutorials!)
+      <br/><br/>
+      
     </div>
   }
 }

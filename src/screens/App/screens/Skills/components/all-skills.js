5e58b6838287e4bc7fb80a74df5b4b658da3fdef
Timothy Angus <timangus321@gmail.com>
2020-09-30 13:38:50 +0200

    skills page rework

---
+++ src/screens/App/screens/Skills/components/all-skills.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Spring } from 'react-spring';
+import {useTransition, animated} from 'react-spring'
+import {Transition} from 'react-spring/renderprops'
+
+
+export default function AllSkills() {
+  return (
+    <h1>hello</h1>
+  )
+    
+}

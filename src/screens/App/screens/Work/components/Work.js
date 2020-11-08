e7ac09bc5d4f3b9df5e82270b1ef104b6b5bd074
Timothy Angus <timangus321@gmail.com>
2020-11-08 13:29:08 +0100

    work on new skills page in work page

--- src/screens/App/screens/Work/components/Work.js
+++ src/screens/App/screens/Work/components/Work.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { Component, useEffect, useRef } from 'react';
+import { Timeline, Tween, SplitWords } from 'react-gsap';
+import { gsap, TweenLite } from 'gsap/all';
 
-function Work() {
-  return <h1>Work</h1>;
+//TODO
+// Redo animation on skills page but this time from a list with keys
+// Then use timeline and tween to have certain keys animate, change color and enlarge
+// to the middle of the right half of the screen
+
+
+class Work extends Component {
+  constructor(props) {
+    super(props);
+ 
+  this.allMySkills = {
+    skillSets: [
+        {frontend: ["React.js", "Javascript", "HTML", "SCSS", "CSS", "APIs"] },
+        {backend: ["Ruby on Rails", "PostgreSQL", "Ruby", "SQL"] },
+        {fullstack: ["React.js", "Javascript", "HTML", "SCSS", "CSS", "APIs","Ruby on Rails", "PostgreSQL", "Ruby", "SQL" ] },
+        {eccomerce: ["Shopify", "Liquid", "Node.js", "AWS", "Lambda"] },
+        {mobile: ["React Navtive", "HTML", "SCSS"] },
+        {softskills: ["German(B1)", "Presenting", "Teaching", "Managing", "Research", "Copywriting", "Musician"] }
+    ]
+  }
+  this.fallingSkills = null;
+ }
+
+ componentDidMount() {
+   this.fallingSkills = TweenLite.to(this.allMySkills, 1, { x: 100, y: 100});
+ }
+
+
+
+
+render() {
+    return (
+        <div className="animated-skills-container">
+
+             {this.allMySkills.skillSets.map(skillset =>
+                  <div key={skillset.id}>{skillset.frontend}</div>
+                )}
+                {this.allMySkills.skillSets.map(skillset =>
+                  <div key={skillset.id}>{skillset.backend}</div>
+                )}
+                {this.allMySkills.skillSets.map(skillset =>
+                  <div key={skillset.id}>{skillset.fullstack}</div>
+                )}
+                {this.allMySkills.skillSets.map(skillset =>
+                  <div key={skillset.id}>{skillset.eccomerce}</div>
+                )}
+                {this.allMySkills.skillSets.map(skillset =>
+                  <div key={skillset.id}>{skillset.mobile}</div>
+                )}
+                {this.allMySkills.skillSets.map(skillset =>
+                  <div key={skillset.id}>{skillset.softskills}</div>
+                )}
+                {/* <div style={{color: 'white'}} ref={div => this.allMySkills = div} /> */}
+
+        </div>
+    )
+}
 }
 
 export default Work;

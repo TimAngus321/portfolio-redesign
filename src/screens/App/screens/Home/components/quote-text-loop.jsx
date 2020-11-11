fcf6d9c6d117014b71ab9e879057d2f0374f91eb
Timothy Angus <timangus321@gmail.com>
2020-11-11 21:34:39 +0100

    quote loop clean up

--- src/screens/App/screens/Home/components/quote-text-loop.jsx
+++ src/screens/App/screens/Home/components/quote-text-loop.jsx
@@ -12,57 +12,37 @@ const Section = cxs("div")({
         '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
 });
 
-const StyledTextLoop = cxs(TextLoop)({
-    display: "flex",
-});
-
 class QuoteLoop extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            options: ["Trade faster", "Increase sales"],
-            interval: 0,
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         interval: 0,
+    //     };
+    // }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                options: [
-                    "Trade faster",
-                    "Increase sales",
-                    "Stock winners",
-                    "Price perfectly",
-                ],
-            });
-            console.log("change options");
-        }, 10000);
-
-        setTimeout(() => {
-            this.setState({
-                interval: 1000,
-            });
-
-            console.log("start");
-            setTimeout(() => {
-                this.setState({
-                    interval: 0,
-                });
-                console.log("stop");
-            }, 10000);
-        }, 5000);
-    }
+    // componentDidMount() {
+    //     setTimeout(() => {
+    //         this.setState({
+    //             interval: 1000,
+    //         });
+    //         setTimeout(() => {
+    //             this.setState({
+    //                 interval: 0,
+    //             });
+    //         }, 10000);
+    //     }, 5000);
+    // }
 
     render() {
-        const { options, interval } = this.state;
+         const interval = this.state;
 
         return (
-            <div>
+            // <div>
                 <Section>
                     <Quotes>
                     <span className="pre-loop-quote">                    
                     <p>Hi</p>
-                    <p>I'm Tim</p>
+                    <p>I'm Tim a</p>
                     </span>
                         <TextLoop className="looping-quote" mask={true} fade={false}>
                             <span>front-end developer</span>
@@ -72,29 +52,7 @@ class QuoteLoop extends React.PureComponent {
                         </TextLoop>
                     </Quotes>
                 </Section>
-                {/* <Section>
-                    <Quotes>
-                    "I am <br/>
-                        <StyledTextLoop mask={true} fade={false} className="transition-quotes">
-                            <span>&nbsp;always</span>
-                            <span>&nbsp;never</span>
-                            <span>&nbsp;continuously</span>
-                        </StyledTextLoop>
-                        <br/>
-                        <StyledTextLoop delay={500} mask={true} fade={false} className="transition-quotes">
-                            <span>&nbsp;learning</span>
-                            <span>&nbsp;satisfied</span>
-                            <span>&nbsp;building </span>
-                        </StyledTextLoop>
-                        <br/>
-                        <StyledTextLoop delay={1000} mask={true} fade={false} className="transition-quotes">
-                            <span>&nbsp;something new."</span>
-                            <span>&nbsp;with my skillset."</span>
-                            <span>&nbsp;projects."</span>
-                        </StyledTextLoop>
-                    </Quotes>
-                </Section> */}
-            </div>
+            // </div>
         );
     }
 }

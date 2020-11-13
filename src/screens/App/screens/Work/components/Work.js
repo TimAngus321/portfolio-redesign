ba71a973bdfc1c0d3b67d50a15e7cc9337536b3c
Timothy Angus <timangus321@gmail.com>
2020-11-13 12:26:28 +0100

    progress on animation

--- src/screens/App/screens/Work/components/Work.js
+++ src/screens/App/screens/Work/components/Work.js
@@ -1,64 +1,68 @@
 import React, { Component, useEffect, useRef } from 'react';
 import { Timeline, Tween, SplitWords } from 'react-gsap';
-import { gsap, TweenLite } from 'gsap/all';
+import { gsap, TweenLite, TimelineLite } from 'gsap/all';
 
 //TODO
 // Redo animation on skills page but this time from a list with keys
 // Then use timeline and tween to have certain keys animate, change color and enlarge
 // to the middle of the right half of the screen
 
+const skillSetStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+}
+
+const individualSkillStyle = {
+  transform: 'rotate(90deg)', 
+  transformOrigin: '0px 0% 0',
+}
 
 class Work extends Component {
-  constructor(props) {
-    super(props);
- 
-  this.allMySkills = {
+  state = {
     skillSets: [
-        {frontend: ["React.js", "Javascript", "HTML", "SCSS", "CSS", "APIs"] },
-        {backend: ["Ruby on Rails", "PostgreSQL", "Ruby", "SQL"] },
-        {fullstack: ["React.js", "Javascript", "HTML", "SCSS", "CSS", "APIs","Ruby on Rails", "PostgreSQL", "Ruby", "SQL" ] },
-        {eccomerce: ["Shopify", "Liquid", "Node.js", "AWS", "Lambda"] },
-        {mobile: ["React Navtive", "HTML", "SCSS"] },
-        {softskills: ["German(B1)", "Presenting", "Teaching", "Managing", "Research", "Copywriting", "Musician"] }
-    ]
-  }
-  this.fallingSkills = null;
- }
+        {id: "frontend", skillset: ["React.js", "Javascript", "HTML", "SCSS", "CSS", "APIs"] },
+        {id: "backend",  skillset: ["Ruby on Rails", "PostgreSQL", "Ruby", "SQL"] },
+        {id: "fullstack", skillset: ["React.js", "Javascript", "HTML", "SCSS", "CSS", "APIs","Ruby on Rails", "PostgreSQL", "Ruby", "SQL" ] },
+        {id: "eccommerce", skillset: ["Shopify", "Liquid", "Node.js", "AWS", "Lambda"] },
+        {id: "mobile", skillset: ["React Navtive", "HTML", "SCSS"] },
+        {id: "softskills", skillset: ["German(B1)", "Presenting", "Teaching", "Managing", "Research", "Copywriting", "Musician"] }
+    ],
+  };
 
- componentDidMount() {
-   this.fallingSkills = TweenLite.to(this.allMySkills, 1, { x: 100, y: 100});
- }
+  // constructor(props) {
+  // super(props)
+  // this.timeline = new TimelineLite;
+  // }
 
+  // componentDidMount(){
+  //   this.timeline.staggerTo( this.skillSets,  0.5, { autoAlpha: 1, y: -20 }, 0.1 );
+  // }
 
+  // useEffect() => {
+  //   const timeLine = gsap.timeline();
+  //   timeline.to('.fullstack', {
+  //     y: '100%',
+  //     duration: 5,
+  //   })
+  // }
 
 
 render() {
     return (
         <div className="animated-skills-container">
-
-             {this.allMySkills.skillSets.map(skillset =>
-                  <div key={skillset.id}>{skillset.frontend}</div>
-                )}
-                {this.allMySkills.skillSets.map(skillset =>
-                  <div key={skillset.id}>{skillset.backend}</div>
-                )}
-                {this.allMySkills.skillSets.map(skillset =>
-                  <div key={skillset.id}>{skillset.fullstack}</div>
-                )}
-                {this.allMySkills.skillSets.map(skillset =>
-                  <div key={skillset.id}>{skillset.eccomerce}</div>
-                )}
-                {this.allMySkills.skillSets.map(skillset =>
-                  <div key={skillset.id}>{skillset.mobile}</div>
-                )}
-                {this.allMySkills.skillSets.map(skillset =>
-                  <div key={skillset.id}>{skillset.softskills}</div>
-                )}
-                {/* <div style={{color: 'white'}} ref={div => this.allMySkills = div} /> */}
-
+          {this.state.skillSets.map(({skillset, id}) => (
+            <div className={id} style={skillSetStyle}>
+              {skillset.map((skillset, skills) =>
+               <p key={skills} style={individualSkillStyle}>
+               {skillset}
+               </p>
+               )} 
+            </div>
+          ))
+        }
         </div>
     )
-}
+  }
 }
 
 export default Work;

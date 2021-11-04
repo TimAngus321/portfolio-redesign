b4e437349669cc98a8024d3c7ec60453c2cb4f61
Timothy Angus <timangus321@gmail.com>
2021-11-04 23:04:17 +0100

    code cleanup

--- src/screens/App/screens/Skills/components/SkillCard.js
+++ src/screens/App/screens/Skills/components/SkillCard.js
@@ -1,21 +1,15 @@
-import React from 'react';
+import React from "react";
 
 class SkillCard extends React.Component {
+  render() {
+    const { name, image, alt } = this.props.skill;
 
-    render() {
-
-        const {name, image, alt} = this.props.skill;
-
-        return (
-           <li className="skill-card">
-               <h3 className="skill-name">
-                {name}
-               </h3>
-            <img src={image} alt={alt} className="skill-image" />
-           </li> 
-        )
-    }
-
-  
+    return (
+      <li className="skill-card">
+        <h3 className="skill-name">{name}</h3>
+        <img src={image} alt={alt} className="skill-image" />
+      </li>
+    );
+  }
 }
 export default SkillCard;
--- src/screens/App/screens/Skills/components/Skills.js
+++ src/screens/App/screens/Skills/components/Skills.js
@@ -1,89 +1,73 @@
-import React from 'react';
-import frontendSkillSet from '../front-end-skills';
-import backendSkillSet from '../backend-skills';
-import shopifySkillSet from '../shopify-skills'
-import mobileSkills from '../mobile-dev-skills';
-import SkillCard from './SkillCard';
+import React from "react";
+import frontendSkillSet from "../front-end-skills";
+import backendSkillSet from "../backend-skills";
+import shopifySkillSet from "../shopify-skills";
+import mobileSkills from "../mobile-dev-skills";
+import SkillCard from "./SkillCard";
 
-  class Skills extends React.Component {
-
-
+class Skills extends React.Component {
   state = {
-    skillSets: frontendSkillSet
-   }
+    skillSets: frontendSkillSet,
+  };
 
   loadFrontEndSkills = () => {
-   this.setState({ skillSets: frontendSkillSet})
+    this.setState({ skillSets: frontendSkillSet });
   };
 
   loadBackendSkills = () => {
-    this.setState({ skillSets: backendSkillSet})
-  }
+    this.setState({ skillSets: backendSkillSet });
+  };
 
   loadShopifySkills = () => {
-    this.setState({ skillSets: shopifySkillSet})
-  }
- 
+    this.setState({ skillSets: shopifySkillSet });
+  };
+
   loadMobileSkills = () => {
-    this.setState({ skillSets: mobileSkills})
-  }
+    this.setState({ skillSets: mobileSkills });
+  };
 
-    //  const { onLoadFrontend, showFrontend, showBackend, showFullstack, showEccomerce, showMobile, showSoftskills } = this.state;
+  //  const { onLoadFrontend, showFrontend, showBackend, showFullstack, showEccomerce, showMobile, showSoftskills } = this.state;
 
-    render() {
+  render() {
     return (
-      <div className="photo-quote">  
-    <div className="skills-container">
-    <div className="select-skills">
-    <h2 className="skills-title">Tech Stack</h2>
-    <div className="about-my-skills">
-      <span 
-      className="about-btn" 
-      onClick={this.loadFrontEndSkills}
-      >
-      &nbsp;Front-end&nbsp;
-      </span>
-      <span
-      className="about-btn" 
-     onClick={this.loadBackendSkills}
-      >
-      &nbsp;Backend&nbsp;
-      </span>
-      <span 
-      className="about-btn" 
-      onClick={this.loadShopifySkills}
-      >
-      &nbsp;Shopify&nbsp;
-      </span>
-      <span
-      className="about-btn" 
-       onClick={this.loadMobileSkills}
-      >
-      &nbsp;Mobile Development&nbsp;
-      </span>
-    </div>
-    </div>
-       <div className="falling-skills-container">
-      
-      <div className="reveal-skills">
-        <div className="selected-skillset">
-          <ul className="skill-cards">
-        {Object.keys(this.state.skillSets).map(key => 
-        <SkillCard 
-        key={key} 
-        skill={this.state.skillSets[key]}
-        image={this.state.skillSets[key]}
-        />
-        )}
-        </ul>
+      <div className="photo-quote">
+        <div className="skills-container">
+          <div className="select-skills">
+            <h2 className="skills-title">Tech Stack</h2>
+            <div className="about-my-skills">
+              <span className="about-btn" onClick={this.loadFrontEndSkills}>
+                &nbsp;Front-end&nbsp;
+              </span>
+              <span className="about-btn" onClick={this.loadBackendSkills}>
+                &nbsp;Backend&nbsp;
+              </span>
+              <span className="about-btn" onClick={this.loadShopifySkills}>
+                &nbsp;Shopify&nbsp;
+              </span>
+              <span className="about-btn" onClick={this.loadMobileSkills}>
+                &nbsp;Mobile Development&nbsp;
+              </span>
+            </div>
+          </div>
+          <div className="falling-skills-container">
+            <div className="reveal-skills">
+              <div className="selected-skillset">
+                <ul className="skill-cards">
+                  {Object.keys(this.state.skillSets).map((key) => (
+                    <SkillCard
+                      key={key}
+                      skill={this.state.skillSets[key]}
+                      image={this.state.skillSets[key]}
+                    />
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
-    </div>
-    )
-    }
+    );
   }
-
+}
 
 export default Skills;

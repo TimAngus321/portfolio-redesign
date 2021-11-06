630d9f24d1fc75e8f41bc404e566c16eb3c189ed
Timothy Angus <timangus321@gmail.com>
2021-11-06 16:16:47 +0100

    majority of about page built

---
+++ src/screens/App/screens/About/work-exp-info.js
@@ -0,0 +1,18 @@
+
+const workExpInfo = {
+    company1: {
+        role: "Full-Stack Developer",
+        compnayName: "Merconic GmbH",
+        workPeriod: " 2020",
+        companyDesc: "Digitial Agency based in Berlin."
+    },
+    company2: {
+        role: "Full-Stack Developer",
+        compnayName: "MilkRun",
+        workPeriod: "2021",
+        companyDesc: "Online delviery company that delviers farm produce straight to your door"
+    }
+
+}
+
+export default workExpInfo;

97775f8794a0bd6f26f21be79aa73a073660f873
Timothy Angus <timangus321@gmail.com>
2021-11-12 00:14:04 +0100

    working on parallax for work page

--- src/screens/App/screens/Work/components/Work.js
+++ src/screens/App/screens/Work/components/Work.js
@@ -1,41 +1,92 @@
-import React from "react";
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import React, {useState} from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import styles from "./styles.module.css";
+import WorkExpCard from './workExpCard';
+import railsProjects from '../rails-projects-data';
+import reactProjects from '../react-projects-data';
+import previousClients from '../work-project-data';
 
 
-const Work = () => {
-    return (
-      <div className="page-container-scroll">
-        <div className="work-container">
-        <Parallax pages={5}>
-        <ParallaxLayer offset={0} speed={0.5} style={{ alignnContent: 'alignCenter', justifyContent: 'center' }}>
+class Work extends React.Component {
 
-          <h3 className="work-title">
-            Work
-          </h3>
+  state = {
+    workExperience: previousClients,
+    offsetIncrease: 1.5,
+  };
+
+
+  render() {
+
+    const alignCenter = { display: "flex", alignItems: "center" };
+
+  return (
+    <div className="page-container-scroll-work">
+      <div className="work-container">
+
+        <Parallax 
+        pages={7}
+        style={{
+          display: 'flex',
+
+        }}>
+          <ParallaxLayer
+            offset={0}
+            speed={0.5}
+            className="work-title"
+            style={{ alignItems: "flex-start", justifyContent: "flex-start" }}
+          >
+            <p className={styles.scrollText}>My Projects</p>
           </ParallaxLayer>
 
-          <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ alignnContent: 'alignCenter', justifyContent: 'flex-start' }}>
-          <div>
-            <p>React Projects</p>
-          </div>
-        </ParallaxLayer>
+          <ParallaxLayer
+            sticky={{ start: 1, end: 7 }}
+            style={{ ...alignCenter, justifyContent: "flex-start" }}
+          >
+            <div className={`${styles.card} ${styles.sticky}`}>
+              <p>Shopify Clients</p>
+            </div>
+          </ParallaxLayer>
 
-        <ParallaxLayer offset={1.5} speed={1.5} style={{ justifyContent: 'flex-end' }}>
-          <div>
-            <p>First Project</p>
-          </div>
-        </ParallaxLayer>
+          {Object.keys(this.state.workExperience).map((key, i) => (
 
-        <ParallaxLayer offset={1.5} speed={1.5} style={{ justifyContent: 'flex-end' }}>
-          <div>
-            <p>Second Project</p>
-          </div>
-        </ParallaxLayer>
+          <ParallaxLayer
+            offset={1.5 + i} 
+            speed={1.5}
+            style={{ ...alignCenter, justifyContent: "flex-end" }}
+          >
+            <div 
+              key={key}
+              className={`${styles.card} ${styles.parallax} ${styles.purple}`}
+            >
+
+            {/* ToDO use a workExpCard component */}
+            {/* <WorkExpCard
+            key={key}>
+              <p>{key}</p>
+            </WorkExpCard> */}
+              <h3 className="client">{previousClients[key].client}</h3> 
+              <h3 className="company">{previousClients[key].company}</h3>
+              <h4 className="project">{previousClients[key].project}</h4>
+              <p className="desc">{previousClients[key].desc}</p>
+              <a href={previousClients[key].link} className="link">
+              {previousClients[key].company}
+              </a>
+             </div>
+          </ParallaxLayer>
+          ))}
+           
 
         </Parallax>
-        </div>
       </div>
+    </div>
     );
-}
+  }
+};
 
 export default Work;
+
+/* <h2 className="client">{this.props.previousClients.client}</h2>
+<h3 className="company"></h3>
+<p className="project"></p>
+<p className="desc"></p>
+<link rel="" href="" /> */
--- src/screens/App/screens/Work/components/workExpCard.js
+++ src/screens/App/screens/Work/components/workExpCard.js
@@ -6,9 +6,17 @@ class WorkExpCard extends React.Component {
 
   render() {
 
+    //  const { client, company, project, desc, link } = this.props.workExperience;
+
     return (
       <li className="work-exp-card">
- 
+        {/* <h3 className="client">{this.workExperience.project1.client}</h3> */}
+        {/* <h3 className="company">{company}</h3>
+        <h4 className="project">{project}</h4>
+        <p className="desc">{desc}</p>
+        <a href="" className="link">
+          {link} */}
+        {/* </a> */}
       </li>
     );
   }

1c14e051aaa0e5ebcbf5a247b145251fb40c497c
Timothy Angus <timangus321@gmail.com>
2021-11-11 15:33:17 +0100

    add react spring package & work on parallax scrolling work page

--- src/screens/App/screens/Work/components/Work.js
+++ src/screens/App/screens/Work/components/Work.js
@@ -1,17 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
-class Work extends Component {
-  render() {
+
+const Work = () => {
     return (
       <div className="page-container-scroll">
         <div className="work-container">
+        <Parallax pages={5}>
+        <ParallaxLayer offset={0} speed={0.5} style={{ alignnContent: 'alignCenter', justifyContent: 'center' }}>
+
           <h3 className="work-title">
             Work
           </h3>
+          </ParallaxLayer>
+
+          <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ alignnContent: 'alignCenter', justifyContent: 'flex-start' }}>
+          <div>
+            <p>React Projects</p>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1.5} speed={1.5} style={{ justifyContent: 'flex-end' }}>
+          <div>
+            <p>First Project</p>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1.5} speed={1.5} style={{ justifyContent: 'flex-end' }}>
+          <div>
+            <p>Second Project</p>
+          </div>
+        </ParallaxLayer>
+
+        </Parallax>
         </div>
       </div>
     );
-  }
 }
 
 export default Work;

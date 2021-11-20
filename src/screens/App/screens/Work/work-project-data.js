ac75a323f6e6095b2349ca6a2b9e84e550318f9a
Timothy Angus <timangus321@gmail.com>
2021-11-20 15:03:26 +0100

    text improvements for parallax scroll cards

--- src/screens/App/screens/Work/work-project-data.js
+++ src/screens/App/screens/Work/work-project-data.js
@@ -10,28 +10,28 @@ const previousClients = {
     project2: {
         client: "Agency: Merconic GmbH",
         company: "Company: Costaboard",
-        project: "Project: Private Shopify Express App deployment, bebugging and customsiation repsonsible for fulillment and shopify theme development.",
-        desc: "Description: Fix issues with prices of certatin options on particular boards and other theme improvements and the deployment, debugging and customisation of a backend Fulfillment Automation App called shopify-wolanski-fulfillment that used AWS Lambda, Express, Node, FTP and Shopify Rest APIs",
+        project: "Project: Deploying, debugging and and customising a Private Shopify Express App repsonsible for fulillment and shopify theme development.",
+        desc: "Description: Fix issues with prices of certatin options on particular boards and other theme improvements and the deployment, debugging and customisation of a backend Fulfillment Automation App called shopify-wolanski-fulfillment that used AWS Cloud Services, Express, Node, FTP and Shopify Rest APIs",
         link: "https://costaboard.com"
     },
     project3: {
         client: "Agency: Merconic GmbH",
         company: "Company: Fleeky",
         project: "Project: Shopify Theme Development",
-        desc: "Description: Implement a redesign by adding components including sticky navbar, responsive card components, sections, fonts and more to Shopify Theme",
+        desc: "Description: Implement a redesign by adding components including: sticky navbars, responsive card components, sections, fonts and more to the Shopify Theme",
         link: "https://fleeky.de"
     },
     project4: {
         client: "Agency: Merconic GmbH",
         company: "Company: Cosmetic Beate Bulang",
         project: "Project: Shopify Theme Development",
-        desc: "Description: Fix Javascript/jQuery issues caused when updating prices when quantities were changed or the coutner for reviews required a reload rather than being updated in the DOM as desired needed to be fixed",
+        desc: "Description: Fix Javascript/jQuery issues caused when updating prices when quantities were changed, the coutner for reviews required and insure information is updating in the DOM rather than being updated only once page has reloaded (was not available from theme)",
         link: "https://www.cosmetic-beate-bulang.de/"
     },
     project5: {
         client: "Full Time Contractor",
         company: "Comapny: MilkRun",
-        project: "Project: Set up a Shopify Store that can deal with multiple cities",
+        project: "Project: Set up a new Shopify Store that can deal with multiple cities",
         desc: "Description: Large project integrating different stores into one store that incldued a lot of use of APIs, 3rd party app customisation and more",
         link: "https://www.localmilkrun.com/"
     }

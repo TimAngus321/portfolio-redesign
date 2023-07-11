d958526ad193644e68f7bab315e367cc90e1a8cd
Timothy Angus <timangus321@gmail.com>
2023-07-11 14:05:16 +0200

    fix manifest, icons and add sw for pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import ReactDOM from 'react-dom';
 import Router from './app/components/App';
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.ts')
+        .then(registration => {
+          console.log('Service Worker registered: ', registration);
+        })
+        .catch(error => {
+          console.log('Service Worker registration failed: ', error);
+        });
+    });
+  }
+
 ReactDOM.render(<Router />, document.querySelector('#root'));
 
 

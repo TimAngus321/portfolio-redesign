b46010f2fdfeeafb611c10b387bdd76dcc121575
Timothy Angus <timangus321@gmail.com>
2023-07-25 22:44:23 +0200

    update contact and side nav

--- src/app/components/NavigationBar/NavBar.tsx
+++ src/app/components/NavigationBar/NavBar.tsx
@@ -10,42 +10,43 @@ import {
 import { Icon } from "@iconify/react";
 import githubFilled from "@iconify/icons-codicon/github-inverted";
 import linkedinRect from "@iconify/icons-brandico/linkedin-rect";
+import styles from './navBarStyle.module.scss';
 
 function NavBar() {
   const navigate = useNavigate();
 
   return (
-    <nav className="navBar">
-      <h1 className="myName" onClick={() => navigate("/")}>
+    <nav className={styles?.navBar}>
+      <h1 className={styles?.myName} onClick={() => navigate("/")}>
         T
       </h1>
       {
-        <ul className="menu">
-          <li className="home" onClick={() => navigate("/")}>
-            <FontAwesomeIcon icon={faHome} className="home-icon" />
+        <ul className={styles?.menu}>
+          <li className={styles?.home} onClick={() => navigate("/")}>
+            <FontAwesomeIcon icon={faHome} className={styles?.homeIcon} />
           </li>
-          <li className="about" onClick={() => navigate("/about")}>
-            <FontAwesomeIcon icon={faUser} className="about-icon" />
+          <li className={styles?.about} onClick={() => navigate("/about")}>
+            <FontAwesomeIcon icon={faUser} className={styles?.aboutIcon} />
           </li>
-          <li className="skills" onClick={() => navigate("/skills")}>
-            <FontAwesomeIcon icon={faCog} className="tech-icon" />
+          <li className={styles?.skills} onClick={() => navigate("/skills")}>
+            <FontAwesomeIcon icon={faCog} className={styles?.techIcon} />
           </li>
-          <li className="work" onClick={() => navigate("/work")}>
-            <FontAwesomeIcon icon={faWrench} className="work-icon" />
+          <li className={styles?.work} onClick={() => navigate("/work")}>
+            <FontAwesomeIcon icon={faWrench} className={styles?.workIcon} />
           </li>
-          <li className="contact" onClick={() => navigate("/contact")}>
-            <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+          <li className={styles?.contact} onClick={() => navigate("/contact")}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles?.contactIcon} />
           </li>
         </ul>
       }
-      <ul className="social-media">
-        <li className="github">
+      <ul className={styles?.socialMedia}>
+        <li className={styles?.github}>
           <a
             href="https://github.com/TimAngus321"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Icon icon={githubFilled} className="github-icon" />
+            <Icon icon={githubFilled} />
           </a>
         </li>
         <li className="linkedin">
@@ -54,7 +55,7 @@ function NavBar() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Icon icon={linkedinRect} className="linkedIn-icon" />
+            <Icon icon={linkedinRect} />
           </a>
         </li>
       </ul>

5a33ecb3b8353c4e7e3177ca24f7e1d86d0c4f69
Timothy Angus <timangus321@gmail.com>
2023-07-30 21:40:52 +0200

    refactor and streamline textloop

---
+++ src/app/pages/Home/components/QuoteLoop.tsx
@@ -0,0 +1,25 @@
+import TextLoop from "react-text-loop";
+import styles from "../style/quoteLoopStyle.module.scss";
+import useQuoteLoop from '../hooks/useQuoteLoop';
+
+const QuoteLoop = () => {
+  const { quoteLoopText } = useQuoteLoop();
+
+  return (
+    <section>
+      <span className={styles?.preLoopQuote}>
+        <p>Hi</p>
+        <p>I'm Tim a</p>
+      </span>
+      <TextLoop
+        className={styles?.quoteLoop}
+        mask={true}
+        fade={true}
+        interval={2000}
+        children={quoteLoopText}
+      />
+    </section>
+  );
+};
+
+export default QuoteLoop;

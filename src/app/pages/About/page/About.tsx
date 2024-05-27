2925d6813c1a3f3dd57e7365561734ffa1476e50
Timothy Angus <timangus321@gmail.com>
2024-05-27 14:37:20 +0100

    about page text update

--- src/app/pages/About/page/About.tsx
+++ src/app/pages/About/page/About.tsx
@@ -20,8 +20,9 @@ const About = () => {
           <p className={styles?.aboutMeBlurb}>
             Le Wagon alumni that learnt full-stack development with the Rails
             framework. Since then I have continued delving deeper into
-            Javascript/Typescript and building projects with React, React
-            Native, Express and Shopify.
+            Javascript/Typescript and building projects with Next, React, Nuxt,
+            Vue, React Native, Express with PostgeSQL or MySQl and E-Commerce 
+            stores with Shopify Themes and Expess, Rails or Grape backends.
             <br />
             <br />
             I've worked fulltime as a full-stack developer for Gavel, MilkRun,

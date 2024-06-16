0882dd3c56ad71f468c6a53f6f0540e8949a9a63
Timothy Angus <timangus321@gmail.com>
2024-06-16 14:06:15 +0200

    text for milkrun api updated

--- src/app/pages/Work/data/ruby-project-data.ts
+++ src/app/pages/Work/data/ruby-project-data.ts
@@ -4,7 +4,7 @@ const rubyProjects = [
 {
     uniqueInfo: "Grape Backend API for a Milkrun Shopify store",
     name: "Milkrun API",
-    desc: "Grape API for updating the shopify store's orders, fullfillments, products and more. It's also used for intergrations with various 3rd party apps and templates for shopify using liquid.",
+    desc: "Grape API for updating the shopify store's orders, fullfillments, products and more. Additionally the API was used for intergrations with various 3rd party apps and various templates for shopify using liquid. Jobs done with Sidekiq",
     techTitle: "Tech Used",
     techStack:
       "Grape, Ruby, Liquid, rSpec, PostgreSQL, Docker, heroku, puma and sidekiq",

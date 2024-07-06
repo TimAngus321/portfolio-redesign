5a2a713747f2d4058d8005c28e07e9193dea39d2
Timothy Angus <timangus321@gmail.com>
2024-07-06 17:28:27 +0200

    contd work on text for services page

--- src/app/pages/Services/data/fullstackDev.ts
+++ src/app/pages/Services/data/fullstackDev.ts
@@ -1,16 +1,28 @@
 const fullstackDev = [
   {
-    title: "Fullstack Development",
-    subTitle: "",
-    desc: "",
+    title: "Full-Stack Development",
+    subTitle: "What does full-stack development cover?",
+    desc: `Full stack development encompasses the complete process of application software development, covering both
+    frontend and backend development. The front end consists of the user interface (or UI), and the back end handles
+    the business logic and application workflows that run behind the scenes. Full stack developers possess the skills and
+    knowledge to work across the entire technology stack, enabling seamless user experiences and developing robust backends.
+    I offer 2 approaches to full-stack development:`,
     sections: [
       {
-        subTitle: "Fulstack Frameworks",
-        desc: `Next, Nuxt and Rails`,
+        subTitle: "Full-Stack Frameworks",
+        desc: `For web apps Full-Stack I use frameworks like Next, Nuxt and Ruby on Rails. These are fantastic
+        options that have everything you need to create robust full-stack apps. Next and Nuxt uses React and Vue 
+        Components to build user interfaces, and Next and Nuxt offers additional backend features and optimizations.
+        Ruby on Rails is  different instead of a component based structure Rails uses Model, View and Controller (MVC)
+        approach and is also a great option. If desired I can swap out Rails frontend and use React instead.`,
       },
       {
-        subTitle: "Seperated Frontend with backend API",
-        desc: `Rails and Express`,
+        subTitle: "Seperated Frontend with Backend API",
+        desc: `While Full-Stack frameworks are great for web apps there are many reasons why you may want to seperate
+        your frontend and backend. For example you may have apps for mobile, browsers and desktop that all need data
+        from your database. In this situation it makes sense to have a separate backend so you can use the same APIs
+        to pull data from your database to all your different apps. This is just an example but there are many other
+        reasons for why you might want to seperate your frontend and backend.`,
       },
     ],
   },

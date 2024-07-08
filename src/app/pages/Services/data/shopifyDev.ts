2c373beac6ababd116227da2918ce3be6c02215c
Timothy Angus <timangus321@gmail.com>
2024-07-08 13:15:12 +0200

    work on Shopify services text

--- src/app/pages/Services/data/shopifyDev.ts
+++ src/app/pages/Services/data/shopifyDev.ts
@@ -1,17 +1,35 @@
 const shopifyDev = [
     {
-      title: "Shopify ",
-      subTitle: "Shopify Consultations",
-      desc: "",
+      title: "Shopify",
+      subTitle: "Shopify Consultation",
+      desc: `Shopify is a great option for an E-Commerce store. It has solved most of the issues that sellers face
+      and has 3rd party apps for anything Shopify hasn't covered. You don't need too much technical experience to set 
+      up and run a Shopify store however there are times you will want certain features, functionality, 
+      control or additional oversight that Shopify doesn't provide out of the box. This is where a developer comes
+      in. Depending on what you want to change will depend on what servies you require.`,
       sections: [
         {
+          subTitle: "Shopify Store Setup",
+          desc: `If `,
+        },
+        {
           subTitle: "Shopify Theme Development",
-          desc: `Developing Shopify themes is a unique development experience and requires expereince to develop them in 
-          a maintainable manner. `,
+          desc: `If you want to change the design, add new sections, or add a new feature in the you will need to work on 
+          Shopify's frontend. When talking about the frontend of Shopify store we're almost always refering to Shopify 
+          Themes. It is possible to create your own custom storefront using Shopify Hydrogen however this is normally
+          for people who have a very distinct vision of what they want the design of their store to be. If you don't fall
+          into this category your frontend is going to be a Shopify theme. Working on Shopify themes is a unique development
+          experience and requires expereince to develop them in a maintainable manner. Creating a proper coding environment
+          using Github, Themekit and Shopify's CLI will allow for a more manageable developer experience. Shopify's Theme
+          Development uses traiditional frontend languages and the templating language Liquid to get data from your
+          store. It's possible to create new sections that you can use normally in your theme editor and using Shopify
+          Proxy to make API calls to your backend.`,
         },
         {
           subTitle: "Shopify App Development",
-          desc: `Rails and Express. Shopiy's APIs are always changing and the documentation doesn't keep up. `,
+          desc: `Interacting with your Shopify store via Shopify's API can be done by setting up your own app. Using your
+          own app you can perfrom CRUD operations on products, customers, orders, inventory, fulfillment, and more. I can
+          offer Shopify apps using Ruby and Node. `,
         },
         {
           subTitle: "3rd Party Apps, Portals and more Customizations",

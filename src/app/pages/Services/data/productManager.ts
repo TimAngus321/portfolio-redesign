bcba9324f9f0d05c2c240b529938e69f964529c0
Timothy Angus <timangus321@gmail.com>
2024-07-16 16:15:14 +0200

    add product manager and technical writer to services page

---
+++ src/app/pages/Services/data/productManager.ts
@@ -0,0 +1,17 @@
+const productMan = [
+  {
+    title: "Product Management",
+    subTitle: "What does Product Management Cover",
+    desc: `A product manager is a professional role that is responsible for the development of products for an 
+      organization, known as the practice of product management. Product managers own the product strategy behind a
+    product, specify its functional requirements, and manage feature releases.`,
+    sections: [
+      {
+        subTitle: "What Product Management experience do I have?",
+        desc: ``,
+      },
+    ],
+  },
+];
+
+export default productMan;

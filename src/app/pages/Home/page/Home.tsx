81ef534aab2f1e6350a3d473d0113b208ff2959c
Timothy Angus <timangus321@gmail.com>
2025-01-31 17:33:47 +0100

    update home page animation

--- src/app/pages/Home/page/Home.tsx
+++ src/app/pages/Home/page/Home.tsx
@@ -29,13 +29,14 @@ const Home = () => {
         top: 0,
         left: 0,
       }}
-      onCreated={({ gl }: any) => {
-        gl.domElement.style.pointerEvents = 'none'; 
-      }}
+      // onCreated={({ gl }: any) => {
+      //   gl.domElement.style.pointerEvents = 'none'; 
+      // }}
     >
       <ShaderGradient
         control='query'
-        urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.2&cAzimuthAngle=180&cDistance=4.9&cPolarAngle=90&cameraZoom=1&color1=%2300fff0%20&color2=%238da0ce&color3=%23ff810a&destination=onCanvas&embedMode=off&envPreset=lobby&format=gif&fov=40&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=0.8&positionX=-1.4&positionY=0&positionZ=0&range=disabled&rangeEnd=40&rangeStart=0&reflection=0.6&rotationX=0&rotationY=10&rotationZ=120&shader=defaults&toggleAxis=false&type=plane&uDensity=2&uFrequency=5.5&uSpeed=0.3&uStrength=1.5&uTime=0&wireframe=false&zoomOut=false"
+        urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%2300fff0&color2=%23ff810a&color3=%238da0ce&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false&zoomOut=false"
+        // urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.2&cAzimuthAngle=180&cDistance=4.9&cPolarAngle=90&cameraZoom=1&color1=%2300fff0%20&color2=%238da0ce&color3=%23ff810a&destination=onCanvas&embedMode=off&envPreset=lobby&format=gif&fov=40&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=0.8&positionX=-1.4&positionY=0&positionZ=0&range=disabled&rangeEnd=40&rangeStart=0&reflection=0.6&rotationX=0&rotationY=10&rotationZ=120&shader=defaults&toggleAxis=false&type=plane&uDensity=2&uFrequency=5.5&uSpeed=0.3&uStrength=1.5&uTime=0&wireframe=false&zoomOut=false"
       />
     </ShaderGradientCanvas>
       <SEO

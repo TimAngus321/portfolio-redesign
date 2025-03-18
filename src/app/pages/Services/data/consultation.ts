dfeb07be4c84dc626096296ae06806a67c603541
Timothy Angus <timangus321@gmail.com>
2025-03-18 15:44:34 +0100

    update consultation section in services

--- src/app/pages/Services/data/consultation.ts
+++ src/app/pages/Services/data/consultation.ts
@@ -3,22 +3,12 @@ const consultation = [
     title: "Consulting",
     subTitle: "General Consultation",
     desc: `If you're having issues with your project getting an outside look can be extremely beneficial. Get in contact if
-    you need any technical assistance.`,
-    sections: [
-      // {
-      //   subTitle: "High Level Consultation",
-      //   desc: ``,
-      // },
-      {
-        subTitle: "Dev Consultation",
-        desc: `If you're unsure about what development services you require or have some questions regarding specific needs
+    you need any technical assistance. If you're unsure about what development services you require or have some questions regarding specific needs
         then we should have a consultation.This is so we can go over the details of your project to figure out what your 
         development needs are, what services you need and if I'm the right fit for your project. The decisions you make 
-        regarding your project's environment, tech stack and infrastructure are vital to allow you to develop, iterate and
-        grow quickly without issue. If you're unsure about your development needs it's essential that you contact an 
-        experienced developer like myself so we can ensure your project is a success.`,
-      },
-    ],
+        regarding your project's UX/UI, tech stack and infrastructure are vital to allow you to create good UX, design, 
+        develop, iterate and grow quickly without issue. If you're unsure about your development needs it's essential
+        that you contact an expert so we can ensure your project is a success.`,
   },
 ];
 

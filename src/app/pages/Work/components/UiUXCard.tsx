eb8ed882aad3c4fcc779f49df33571f7b04ab404
Timothy Angus <timangus321@gmail.com>
2025-03-20 17:02:14 +0100

    update work page with UX/UI case studies

---
+++ src/app/pages/Work/components/UiUXCard.tsx
@@ -0,0 +1,41 @@
+import { personalProj } from "../types/workTypes";
+import styles from "../style/projCardStyle.module.scss";
+import useProjCard from "../hooks/useProjCard";
+import projMobileStyle from "../style/projCardStyle.module.scss";
+
+const UiUXCard = (props: personalProj) => {
+    const {
+      link,
+      name,
+      desc,
+      techTitle,
+      techStack,
+      img,
+      uniqueInfo,
+    } = useProjCard(props);
+
+  return (
+    <div className={projMobileStyle?.mobileCard}>
+      <a
+        href={link}
+        className={styles?.projCard}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={img} alt={name} className={styles?.projCardImg} />
+        <div className={styles?.scrollCards}>
+          <div className={styles?.companyInfo}>
+            <h4 className={styles?.uniqueInfo}>{uniqueInfo}</h4>
+            <h4 className={styles?.company}>{name}</h4>
+            <p className={styles?.companyName}>{desc}</p>
+          </div>
+          <div className={styles?.projectInfo}>
+            <h4 className={styles?.project}>{techTitle}</h4>
+            <p className={styles?.projectInfo}>{techStack}</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+export default UiUXCard;

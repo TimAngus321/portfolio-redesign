fd7275cc71de1a266c7f5d46f860adea9a41bbdc
Timothy Angus <timangus321@gmail.com>
2025-11-02 19:32:38 +0000

    add png high quality images and update react projects to work project

--- src/app/pages/Work/data/react-projects-data.ts
+++ src/app/pages/Work/data/react-projects-data.ts
@@ -1,8 +1,8 @@
-import pokemonAppImg from "../../../../assets/workImages/pokemonAppImg.webp";
-import gavelMobileImg from "../../../../assets/workImages/gavelMobileImg.webp";
-import gavelWebImg from "../../../../assets/workImages/gavelWebImg.webp";
-import reactFlatsImg from "../../../../assets/workImages/reactFlatsImg.webp";
-import personalPortfolioImg from "../../../../assets/workImages/personalPortfolioImg.webp";
+import pokemonAppImg from "../../../../assets/workImages/pokemonApp.png";
+import gavelMobileImg from "../../../../assets/workImages/gavelMobileImg.png";
+import gavelWebImg from "../../../../assets/workImages/gavelWebImg.png";
+import reactFlatsImg from "../../../../assets/workImages/reactFlatsImg.png";
+import personalPortfolioImg from "../../../../assets/workImages/portfolioImg.png";
 
 const reactProjects = [
   {
